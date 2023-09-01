e59530b3571ab821a55af104ccb5dcde5c2cf93a
Alyona <polova_alyona0907@ukr.net>
2023-09-01 18:03:20 +0200

    task 01

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { Profile } from "./Profile/Profile";
+import user from "../user.json";
+
+export const App = () => {
+  return (
+    <>
+      1 - Profile in social media
+      <Profile user={user} />
+    </>
+  );
+};
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+import { ProfileCard } from "../ProfileCard/ProfileCard";
+import PropTypes from "prop-types";
+
+export const Profile = ({ user }) => {
+  return (
+    <div className="profile-card">
+      <ProfileCard
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
